cf1017d86daad7e86d4975df3621233b468c46dc
Piotr Mitros <piotr@mitros.org>
2025-05-28 23:01:17 -0400

    UseDynamic works

--- src/components/blocks/_UseDynamic.jsx
+++ src/components/blocks/_UseDynamic.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { render } from '@/lib/render';
 
-export function _Use({ kids, idMap }) {
-  const { resolvedId } = kids || {};
-
-  if (!resolvedId) {
+export function _UseDynamic({ target, idMap }) {
+  if (!target) {
     return <pre className="text-red-500">[Missing &lt;Use&gt; resolution]</pre>;
   }
 
-  return render({ node: resolvedId, idMap });
+  return render({ node: target, idMap });
 }

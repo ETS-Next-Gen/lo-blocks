ab4e8ecd8a822d451e2a9dbfd9bbb343bffd3cca
Piotr Mitros <piotr@mitros.org>
2025-07-24 01:44:19 -0400

    Removed debug code; documented bugs

--- src/components/blocks/KeyGrader.js
+++ src/components/blocks/KeyGrader.js
@@ -3,10 +3,19 @@ import * as parsers from '@/lib/content/parsers';
 import * as blocks from '@/lib/blocks';
 import _Noop from './_Noop';
 import * as state from '@/lib/state';
-import gradeKeySelected from './gradeKeySelected.js';
+import { CORRECTNESS } from '../../lib/blocks/correctness.js';
 
 export const fields = state.fields(['correct', 'message']);
 
+function gradeKeySelected(props, { input }) {
+  const selected = input?.value ?? '';
+  const choice = input?.choices?.find(c => c.id === selected);
+  // TODO: Handle the situation where nothing is selected (and possible invalid inputs; e.g. if there's a bug)
+  const correct = choice?.tag === 'Key'
+    ? CORRECTNESS.CORRECT
+    : CORRECTNESS.INCORRECT;
+  return { correct, message: '' };
+}
 
 const KeyGrader = blocks.test({
   ...parsers.blocks,

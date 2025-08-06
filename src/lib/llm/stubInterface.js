754f290a1e946830ed11efe4a62164946e75ee15
Piotr Mitros <piotr@mitros.org>
2025-08-06 05:58:18 -0400

    Removing old LLM code

--- src/lib/llm/stubInterface.js
+++
@@ -1,9 +0,0 @@
-// src/lib/llm/stubInterface.js
-
-// Part of the OBSOLETE / DEPRECATED API
-//
-// We still need an equivalent stub for the new API
-
-export async function listChatCompletions() {
-  return "Great job! I have no more feedback.";
-}

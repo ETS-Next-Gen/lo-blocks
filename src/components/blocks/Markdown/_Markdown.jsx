58af72cba61b02955f4ba2fd1aafb93e4898c347
Piotr Mitros <piotr@mitros.org>
2025-08-11 08:02:49 -0400

    Markdown plugin for tables, etc.

--- src/components/blocks/Markdown/_Markdown.jsx
+++ src/components/blocks/Markdown/_Markdown.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 export function _Markdown( props ) {
   const { kids } = props;
-  return <ReactMarkdown>{kids}</ReactMarkdown>;
+  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{kids}</ReactMarkdown>;
 }

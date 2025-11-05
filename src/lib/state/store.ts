3b32c08873f3630a29c7df9ab2680ee64f1acaed
Bradley Erickson <bbwe24@gmail.com>
2025-11-05 16:04:58 -0500

    added websocket logger to enable fetch and saving of data from server (#93)
    * added websocket logger to enable fetch and saving of data from server
    
    * cleaned up websocket

--- src/lib/state/store.ts
+++ src/lib/state/store.ts
@@ -18,9 +18,12 @@ import * as reduxLogger from 'lo_event/lo_event/reduxLogger.js';
 import * as lo_event from 'lo_event';
 import * as debug from 'lo_event/lo_event/debugLog.js';
 import { consoleLogger } from 'lo_event/lo_event/consoleLogger.js';
+import { websocketLogger } from 'lo_event/lo_event/websocketLogger.js';
 import { scopes, Scope } from './scopes';
 import type { FieldInfo, Fields } from '../types';
 
+// TODO this ought to come from settings instead
+const WEBSOCKET_URL = 'ws://localhost:8888/wsapi/in/'
 const initialState = {
   component: {},
   componentSetting: {},
@@ -106,14 +109,16 @@ function configureStore({ extraFields = [] }: { extraFields?: ExtraFieldsParam }
   lo_event.init(
     'org.ets.sba',
     '0.0.1',
-    [consoleLogger(), reduxLogger.reduxLogger([], {})],
+    [consoleLogger(), reduxLogger.reduxLogger([], {}), websocketLogger(WEBSOCKET_URL)],
     {
       debugLevel: debug.LEVEL.EXTENDED,
       debugDest: [debug.LOG_OUTPUT.CONSOLE],
       useDisabler: false,
+      sendBrowserInfo: true,
       queueType: lo_event.QueueType.IN_MEMORY
     }
   );
+  lo_event.setFieldSet([{ activity: 'lo-blocks' }]);
   lo_event.go();
   return reduxLogger.store;
 }
